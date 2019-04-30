631795fb337610ac400c2f14024c99f0aa71cd5d
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2019-04-30 17:21:36 +0000

    Update finnaleid2.0.py

--- finnaleid2.0.py
+++ finnaleid2.0.py
@@ -11,8 +11,7 @@ def path(listi,ts,ls):
         return list(k[key].keys())[0]
     if len(listi)-2 == ls:
         gildi = int(listi[ls][ts]) + max(int(listi[ls+1][ts+1]), int(listi[ls+1][ts]))
-        k[key] = {gildi:[listi[ls][ts]+","+str(max(int(listi[ls+1][ts+1]), int(listi[ls+1][ts]))),str(ts)+","+str(ls)]}
-        #print(k)
+        k[key] = {gildi:[listi[ls][ts]+","+str(max(int(listi[ls+1][ts+1]), int(listi[ls+1][ts]))),str(ts)+","+str(ls)+":"+str(ts+1)+","+str(ls+1)]}
         return gildi
     else:
         s1 = path(listi,ts,ls+1)
@@ -22,7 +21,6 @@ def path(listi,ts,ls):
             k[key] = {gildi:[listi[ls][ts]+","+k[str(ts)+","+str(ls+1)][s1][0],str(ts)+","+str(ls)+":"+k[str(ts)+","+str(ls+1)][s1][1]]}
         elif s2 >= s1:
             k[key] = {gildi:[listi[ls][ts]+","+k[str(ts+1)+","+str(ls+1)][s2][0],str(ts)+","+str(ls)+":"+k[str(ts+1)+","+str(ls+1)][s2][1]]}
-        #print(k)
         return gildi
 
 
@@ -34,7 +32,9 @@ for x in k.values():
     if s == list(x.keys())[0]:
         listinn =x[s]
         print(listinn)
-        listi2 = x[s][0].split(",")
+        listi2 = listinn[0].split(",")
+        stadalisti = listinn[1].split(":")
+        print(stadalisti)
         for x in listi2:
             summa+=int(x)
         print(summa)
@@ -43,7 +43,11 @@ for x in range(len(listi)):
     t = listi2[x]
     for i in range(len(listi[x])):
         if listi[x][i] == t:
-            print("["+listi[x][i]+"] ",end="")
+            xgildid = int(stadalisti[x].split(",")[0])
+            if xgildid == i:
+                print("["+listi[x][i]+"] ",end="")
+            else:
+                print(str(listi[x][i]) + " ", end="")
         else:
             print(str(listi[x][i])+" ",end="")
     print("")
